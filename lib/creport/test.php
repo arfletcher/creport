1c5fe7e964038dc246eb319b015046bd89e52fcf
root <root@caddy.bifb.org>
2020-02-14 14:24:11 -0500

    Added php script support

---
+++ lib/creport/test.php
@@ -0,0 +1,14 @@
+#!/usr/bin/php
+<?php
+
+## title creport test script title
+
+// $argv = array('coffee', 'brown', 'caffeine');
+
+echo "php test script ", join(" ", $argv), "\n";
+
+// loop through each element in the $argv array
+// foreach($argv as $value) { echo "$value\n"; }
+
+?>
+

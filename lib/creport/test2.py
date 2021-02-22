e0c104adac01f0a55fc9d860154df5f77bcce4fc
root <root@caddy.bifb.org>
2021-02-22 16:55:51 -0500

    added support for python3, python2

---
+++ lib/creport/test2.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python2
+
+# creport test script
+## clushopt --pick=5
+## clush -a
+## groupby reports
+## title Python Test Script
+
+import sys
+
+#sys.argv[0] = "fred";
+print "Test script ", str(sys.argv)
+
+
